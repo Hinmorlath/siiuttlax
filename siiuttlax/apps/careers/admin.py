5ae48d18699742b78f2cd443b945872e54ecef4b
Emmafloh13 <emmafh342@gmail.com>
2024-07-12 12:49:48 -0600

    Problemas resueltos

--- siiuttlax/apps/careers/admin.py
+++ siiuttlax/apps/careers/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
+
 from .models import Career
+from .models import Subject
 
-
+# Register your models here.
 @admin.register(Career)
-class careersAdmin(admin.ModelAdmin):
-    list_display = ('short_name', 'level', 'name')
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'level', 'name']
     ordering = ['level', 'short_name']
 
-
-#@admin.register(Subject)
-#class SubjectAdmin(admin.ModelAdmin):
-    #list_display=['name', 'career', 'semester', 'sema']
-    #list_filter = ['carrer', 'semester']
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'career', 'semester', 'semanal_hours', 'total_hours']
+    list_filter = ['career', 'semester']
--- siiuttlax/apps/careers/models.py
+++ siiuttlax/apps/careers/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-# Create your models here.
 from apps.period.models import Semester
+
 
 class Career(models.Model):
     LEVELS = [
@@ -10,6 +10,7 @@ class Career(models.Model):
         ('M', 'Maestría'),
     ]
     level = models.CharField(
+        
             verbose_name='Nivel',
             max_length=10,
             choices = LEVELS,    
@@ -23,10 +24,11 @@ class Career(models.Model):
             max_length=20,    
         )
     status = models.BooleanField(verbose_name='Estado', default=True)
-    year_plan = models.CharField(verbose_name='Plan de estudios', max_length=4, blank=True, null=True)
+    #year_plan = models.CharField(verbose_name='Plan de estudios', max_length=4, blank=True, null=True)
     
     def __str__(self):
         return self.short_name
+        
     
     class Meta:
         verbose_name = 'carrera'
@@ -36,13 +38,14 @@ class Career(models.Model):
 class Subject(models.Model):
     name = models.CharField(verbose_name='Nombre', max_length=200)
     career = models.ForeignKey(Career, on_delete=models.CASCADE, verbose_name='Carrera')
-    #semester = models.ForeignKey(Semester, on_delete=models.CASCADE, verbose_name='Cuatrimestre')
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, verbose_name='Cuatrimestre')
     teorical_hours = models.IntegerField(verbose_name='Horas teóricas', default=0)
     practical_hours = models.IntegerField(verbose_name='Horas prácticas', default=0)
     total_hours = models.IntegerField(verbose_name='Horas totales', default=0)
     semanal_hours = models.IntegerField(verbose_name='Horas semanales', default=0)
     file = models.CharField(verbose_name='Archivo', max_length=200, blank=True, null=True)
-    #file = models.FileField(verbose_name='Archivo', upload_to='subjects') ## Definir donde se almacenan archivos AWS S3
+    file = models.FileField(verbose_name='Archivo', upload_to='subjects')
+    
 
 
     def __str__(self):
