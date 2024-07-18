c03a8dd89a0a98d7f205552bad58d460518f5bc6
Emmafloh13 <emmafh342@gmail.com>
2024-07-18 13:20:57 -0600

    Agregando formulario para academy

--- siiuttlax/apps/academy/views.py
+++ siiuttlax/apps/academy/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import ProfessorForm, StudentForm
 
 # Create your views here.
+def create_professor(request):
+    form = ProfessorForm()
+    return render(request,
+                  'academy/create_professor.html',
+                  {'form': form})
+
+def create_student(request):
+    form = StudentForm()
+    return render(request,
+                  'academy/create_student.html',
+                  {'form': form})
+    
